3a52807d2b8abc08a7a62c6d859da835eab0ebe8
hyeg0121 <hyeg0121@naver.com>
2023-04-24 21:56:51 +0900

    selector에서 값을 가져오기 (함수로)

--- js/index.js
+++ js/index.js
@@ -1,14 +1,18 @@
 const inputSelector = document.querySelector('#input_selector');
 const outputSelector = document.querySelector('#output_selector');
-const changeBtn = document.querySelector('changeBtn');
+const changeBtn = document.querySelector('#changeBtn');
+
+function getValue(element){
+    let value = element.value;
+    return value;
+}
 
 inputSelector.addEventListener('change', function() {
-    let selectedValue = inputSelector.value;
-    console.log(selectedValue);
+    getValue(inputSelector);
 });
 
 outputSelector.addEventListener('change', function() {
-    let selectedValue = outputSelector.value;
-    console.log(selectedValue);
+    getValue(outputSelector);
 });
+
 
