b925878dc44259dee86c0e1c78108d62675a5a44
hyeg0121 <hyeg0121@naver.com>
2023-04-27 22:10:25 +0900

    outputtext 가져오기

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
 const inputSelector = document.querySelector('#input_selector');
 const outputSelector = document.querySelector('#output_selector');
 const inputText = document.querySelector('#input_text');
+const outputText = document.querySelector('#output_text');
 const changeBtn = document.querySelector('#changeBtn');
 
 function getValue(element){
     let value = element.value;
     return value;
 }
+
+
 
 inputSelector.addEventListener('change', function() {
     getValue(inputSelector);
@@ -16,6 +19,3 @@ outputSelector.addEventListener('change', function() {
     getValue(outputSelector);
 });
 
-changeBtn.addEventListener('click', function() {
-    console.log(inputText.value);
-} );
