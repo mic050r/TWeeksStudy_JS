40b1e643d9e1b8b1e8cc367e7a9457b7ebf4e54a
mic050r <103114387+mic050r@users.noreply.github.com>
2023-06-06 01:30:31 +0900

    [Add] : querySelector()메소드를 이용하여 form 요소에 접근

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// querySelector()메소드를 이용하여 form 요소에 접근(id는 #)
+const inputSelector = document.querySelector("#input_selector");
+const outputSelector = document.querySelector("#output_selector");
+const inputText = document.querySelector("#input_text");
+const outputText = document.querySelector("#output_text");
